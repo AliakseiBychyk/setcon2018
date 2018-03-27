7c876de9e28eedba5274dc9968618cf921adb219
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-27 16:22:55 +0300

    main page

--- src/components/App.js
+++ src/components/App.js
@@ -62,6 +62,24 @@ class App extends Component {
           </div>
         </section>
 
+        <section className="conference-description background-white counter-diagonal--top-white main-diagonal--top-white">
+          <p className="conference-description__text">Ежегодная профессиональная инженерная конференция, цель которой создать площадку для активного общения и обмена знаниями. Эксперты из разных сфер IT делятся опытом разработки и имплементации программных продуктов и сервисов.</p>
+        </section>
+        <section className="venue-section background-rose">
+          <div className="venue-section__map" >
+            <div className="venue-section__map__action" id="map"></div>
+          </div>
+          <div className="venue-section__description">
+            <p className="venue-section__description__item">iBC Бизнес-центр - Intelligent Business Center. </p>
+            <p className="venue-section__description__item">Расположен в тихом и живописном месте Бреста, в здании кафе-клуба 11:11, по адресу ул. Л-та Рябцева, 124.</p>
+            <p className="venue-section__description__item">Вход со стороны ул. Л-та Рябцева.</p>
+          </div>
+        </section>
+        <section className="start-time-section background-dark-red counter-diagonal--top-dark-red">
+          <p className="start-time-section__text">Начало в 11:00</p>
+        </section>
+
+
 
       </div>
     )
