700a760a20a1f693c73aab212b71374c009af53c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-28 12:18:38 +0300

    speaker images

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -2,14 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-
 const Speaker = ({name, subject, description, image}) => {
+  const speakerImage = image ? require('../img/'+ image) : ''
+
   return (
     <div>
       <h1>{name}</h1>
       <h2>{subject}</h2>
       <p>{description}</p>
-      <img src={"../img/"+ image} className="speaker_image"/>
+      <img src={speakerImage} className="speaker_image"/>
     </div>
   )
 }
