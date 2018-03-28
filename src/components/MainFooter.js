78a2df67501070ee41ba5a8e2130e708e914f964
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-28 15:52:37 +0300

    images reorganization

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import epamLogo from '../img/epam_logo.png'
 import myITLogo from '../img/MyIT_logo_Big.png'
 import itSharkLogo from '../img/it_shark_logo.png'
+import seavus from '../img/SeavusLogo-Color.png'
 
 
 const MainFooter = () => {
@@ -15,6 +16,7 @@ const MainFooter = () => {
           <a href="https://www.epam.com/" target="_blank"><img src={epamLogo} className="epam_logo" /></a>
           <a href="https://it-shark.pro/" target="_blank"><img src={itSharkLogo} className="epam_logo" /></a>
           <a href="https://myit.by/" target="_blank"><img src={myITLogo} className="epam_logo" /></a>
+          <a href="https://seavus.com/" target="_blank"><img src={seavus} className="seavus_logo" /></a>
         </div>
       </div>
       <div className="main-footer__info background-dark-red counter-diagonal--top-dark-red">
