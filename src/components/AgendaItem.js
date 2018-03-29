778e98df799a0bd9982276e7b020a2d0b562cccd
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-29 18:37:02 +0300

    agenda page

--- src/components/AgendaItem.js
+++ src/components/AgendaItem.js
@@ -3,31 +3,34 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 
-const AgendaItem = ({name_r, subject, description, time, image}) => {
+const AgendaItem = ({name_r, subject, description_alt, time, image}) => {
   return (
-    <div className="time-table-section">
-      <h3>{time}</h3>
-      <div className="time-table-section__container">
-        <div className="time-table-section__container_inner">
-          <span class="time-table-section__event">{subject}</span>
-          <div class="time-table-section__event__details__container">
-            <div class="time-table-section__event__details">
-              <div>
-                <div class="time-table-section__event__person">
-                  <span class="time-table-section__event__person__avatar">
-                    <img src={'../img/'+ image} alt={"avatar for ", name}/>
-                  </span>
-                  <h2>{name_r}</h2>
-                  <div class="time-table-section__event__person__bio">
-                    {description}
+
+      <div className="time-table-section">
+        <h3>{time}</h3>
+        
+        <div className="time-table-section__container">
+          <div className="time-table-section__container_inner">
+            <span className="time-table-section__event">{subject}</span>
+            <div className="time-table-section__event__details__container">
+              <div className="time-table-section__event__details">
+                <div>
+                  <div className="time-table-section__event__person">
+                    <span className="time-table-section__event__person__avatar">
+                      <img src={'../img/'+ image} alt={"avatar for ", name}/>
+                    </span>
+                    <h2>{name_r}</h2>
+                    <div className="time-table-section__event__person__bio">
+                      {description_alt}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div>  
+
   )
 }
 
