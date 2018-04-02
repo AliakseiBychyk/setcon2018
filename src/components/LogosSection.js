432786de503d5a959154e7ce5ff995b6ea3c6850
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2018-04-02 14:31:00 +0300

    Refactor logos and add slider

--- src/components/LogosSection.js
+++ src/components/LogosSection.js
@@ -4,25 +4,27 @@ import myITLogo from '../img/MyIT_logo_Big.png'
 import itSharkLogo from '../img/IT_Shark-inversion.png'
 import seavus from '../img/SeavusLogo-Color.png'
 import smartexLogo from '../img/smartex_logo.svg'
+import LogoSlider from './LogoSlider';
 
 const LogosSection = () => {
   return (
     <section className="logos-section">
-      <div className="logos">
+      <LogoSlider />
+      <div className="logos-section__logos">
         <a href="https://www.epam.com/" target="_blank">
-          <img src={epamLogo} className="epam_logo" />
+          <img src={epamLogo} className="logo" />
         </a>
         <a href="https://it-shark.pro/" target="_blank">
-          <img src={itSharkLogo} className="epam_logo" />
+          <img src={itSharkLogo} className="logo" />
         </a>
         <a href="https://myit.by/" target="_blank">
-          <img src={myITLogo} className="epam_logo myit_logo" />
+          <img src={myITLogo} className="logo" />
         </a>
         <a href="https://seavus.com/" target="_blank">
-          <img src={seavus} className="seavus_logo" />
+          <img src={seavus} className="logo" />
         </a>
         <a href="https://smartexlab.com/" target="_blank">
-          <img src={smartexLogo} className="epam_logo smartex_logo" />
+          <img src={smartexLogo} className="logo smartex_logo" />
         </a>
       </div>
     </section>
