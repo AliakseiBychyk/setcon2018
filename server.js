38d296acba206005988a96abd61fa9acf9adbb4a
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-04-03 11:12:24 +0300

    ssr clear obsolete code

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-// app.get('/*', (req, res) => {
-//   res.render('index', {
-//     content: 'Setcon 2018 Brest'
-//   })
-// })
-
 app.get('/*', serverRender)
 
 app.listen(config.port, () => {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-// import App from './components/App'
-// import Agenda from './components/Agenda'
-// import Speakers from './components/Speakers'
 import AppRoutes from './routes/AppRoutes'
 import 'normalize.css/normalize.css'
 import './styles/main.scss'
-
-// render(
-//   <Router>
-//     <Switch>
-//       <Route exact path="/" component={App} />
-//       <Route path="/agenda" component={Agenda} />
-//       <Route path="/speakers" component={Speakers} />
-//     </Switch>
-//   </Router>,
-//   document.getElementById('root')
-// )
 
 render(
   <Router>
