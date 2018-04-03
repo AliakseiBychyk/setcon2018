e0f1d81e6b5322cfa63e0a9e459994766cf14bb8
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-04-03 11:26:14 +0300

    improvement

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom'
 const MainFooter = () => {
   return (
     <footer className="main-footer">
-      <div className="main-footer__sloping-block"></div>
       <div className="main-footer__info background-dark-red">
         <span>© 2018. SETCON Brest. | Пишите нам</span>
           <a className="main-footer__info__email" href="mailto:it.shark.pro@gmail.com">it.shark.pro@gmail.com</a>
